71b7987d6e9ef20f6949a1e297c9effc9923dfb3
minjun0423 <162403690+minjun0423@users.noreply.github.com>
2024-06-06 16:37:45 +0900

    test

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
 @app.get("/")
-def root():
-    return {"message":"hello root!1"}
+def read_root():
+    return {"message": "Subway Info"}
 
-@app.get("/home")
-def home():
-    return {"message":"home!"}
+@app.get("/subway/{station}")
+def get_real_time_arrival_info(station: str):
+    url = f"{BASE_URL}/{API_KEY}/json/realtimeStationArrival/0/5/{station}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()  # JSON 형식 변환 데이터
+        
+        processed_data = []  # 데이터 가공
+        for arrival_info in data.get("realtimeArrivalList", []):
+            processed_data.append(ArrivalInfo(
+                열차번호=arrival_info.get("btrainNo", ""),
+                호선=arrival_info.get("subwayId", ""),
+                상하행선구분=arrival_info.get("updnLine", ""),
+                지하철_위치=arrival_info.get("arvlMsg3", ""),
+                지하철_도착까지=arrival_info.get("arvlMsg2", "")
+            ))
+        return processed_data
+    except Exception as e:
+        return {"error": str(e)}
